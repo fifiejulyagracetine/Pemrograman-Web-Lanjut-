458944f139d550ce9ad1525f5a03f3cecbf23850
fifiejulyagracetine <90020148+fifiejulyagracetine@users.noreply.github.com>
2022-12-06 12:55:06 +0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="wrapper">
+      <div class="sidebar">
+          <h2>Daftar Menu</h2>
+          <ul>
+              <li><a href="#"><i class="fas fa-home"></i>Makanan</a></li>
+              <li><a href="#"><i class="fas fa-user"></i>Minuman</a></li>
+              <li><a href="#"><i class="fas fa-address-card"></i>Snack</a></li>
+  
+          </ul> 
+          <div class="social_media">
+            <a href="#"><i class="fab fa-facebook-f"></i></a>
+            <a href="#"><i class="fab fa-twitter"></i></a>
+            <a href="#"><i class="fab fa-instagram"></i></a>
+        </div>
+      </div>
+      <div class="main_content">
+          <div class="header">Welcome!! Have a nice day.</div>  
+          <div class="info">
+            <div>Selamat Datang Di Aplikasi Mac Donal, </div>
+            <div>Silahkan Pesan Makanan Anda </div>
+        </div>
+      </div>
+  </div>
+  </template>
+  
+  <script>
+  
+  
+  
+  </script>
+  
+  <style>
+  #app {
+    font-family: 'Times New Roman', Times, serif, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: left;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  </style>
+  
---
+++ src/main.js
@@ -0,0 +1,4 @@
+import { createApp } from 'vue'
+import App from './project_01_71190447.vue'
+
+createApp(App).mount('#app')
